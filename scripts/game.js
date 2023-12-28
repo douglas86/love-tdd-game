e7b883019101dacab07bce697b6fa304c08ac00f
douglas <douglasmaxton@gmail.com>
2023-12-28 16:03:06 +0000

    created a showTurn function

--- scripts/game.js
+++ scripts/game.js
@@ -16,6 +16,7 @@ function newGame() {
 function addTurn() {
     game.playerMoves = []
     game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))])
+    showTurns()
 }
 
 function lightsOn(circ) {
@@ -25,8 +26,19 @@ function lightsOn(circ) {
     }, 400)
 }
 
+function showTurns() {
+    game.turnNumber = 0
+    let turns = setInterval(() => {
+        lightsOn(game.currentGame[game.turnNumber])
+        game.turnNumber++
+        if (game.turnNumber >= game.currentGame.length){
+            clearInterval(turns)
+        }
+    }, 800)
+}
+
 function showScore() {
     document.getElementById('score').innerText = game.score
 }
 
-module.exports = {game, newGame, showScore, addTurn, lightsOn}
+module.exports = {game, newGame, showScore, addTurn, lightsOn, showTurns}
