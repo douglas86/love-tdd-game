987693f9a8c2481a6de35684838b137a6df8aca8
douglas <douglasmaxton@gmail.com>
2023-12-28 16:42:30 +0000

    wrote test to check if no btn was pressed during sequence

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -102,4 +102,14 @@ describe('gameplay works correctly', () => {
         playerTurn()
         expect(window.alert).toBeCalledWith('Wrong move!')
     })
+    test('should toggle turnInProgress to true', () => {
+        showTurns()
+        expect(game.turnInProgress).toBe(true)
+    })
+    test('clicking during computer sequence should fail', () => {
+        showTurns()
+        game.lastButton = ""
+        document.getElementById('button2').click()
+        expect(game.lastButton).toEqual("")
+    })
 });
